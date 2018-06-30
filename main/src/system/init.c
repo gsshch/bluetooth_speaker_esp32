aab92950b26161b2e9d7e80c2f1c95f05de35d78
redchenjs <redchenjs@live.com>
2018-06-30 15:35:22 +0800

    init: remove spiffs

--- main/src/system/init.c
+++ main/src/system/init.c
@@ -9,7 +9,6 @@
 #include "device/spi.h"
 #include "device/i2s.h"
 #include "device/gpio.h"
-#include "device/spiffs.h"
 
 #include "driver/led.h"
 
@@ -23,7 +22,6 @@ void device_init(void)
 #if !defined(CONFIG_I2S_OUTPUT_INTERNAL_DAC) && !defined(CONFIG_I2S_OUTPUT_PDM)
     gpio0_init();
 #endif
-    spiffs0_init();
 }
 
 void driver_init(void)
